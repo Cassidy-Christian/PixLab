7f1bd06a4d524de4e501798bde7863b64f355bad
Cassidy Christian <cassidysparkles@icloud.com>
2020-02-10 07:35:48 -0700

    Edit Acitivity5

--- src/Picture.java
+++ src/Picture.java
@@ -94,6 +94,26 @@ public class Picture extends SimplePicture {
 			}
 		}
 	}
+	public void negate(){
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setBlue(255-pixelObj.getBlue());
+				pixelObj.setGreen(255-pixelObj.getGreen());
+				pixelObj.setRed(255-pixelObj.getRed());
+			}
+		}
+	}
+	
+	public void keepOnlyBlue() {
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setGreen(0);
+				pixelObj.setRed(0); 
+			}
+		}
+	}
 
 	/**
 	 * Method that mirrors the picture around a vertical mirror in the center of
--- src/PictureTester.java
+++ src/PictureTester.java
@@ -16,6 +16,18 @@ public class PictureTester
     beach.zeroBlue();
     beach.explore();
   }
+  public static void testKeepOnlyBlue(){
+    Picture beach = new Picture("beach.jpg");
+    beach.explore(); 
+    beach.keepOnlyBlue(); 
+    beach.explore();  
+  }
+  public static void testNegate(){
+    Picture beach= new Picture("beach.jpg");
+    beach.explore(); 
+    beach.negate(); 
+    beach.explore(); 
+  }
   
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
@@ -58,27 +70,29 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    testZeroBlue();
-    //testKeepOnlyBlue();
-    //testKeepOnlyRed();
-    //testKeepOnlyGreen();
-    //testNegate();
-    //testGrayscale();
-    //testFixUnderwater();
-    //testMirrorVertical();
-    //testMirrorTemple();
-    //testMirrorArms();
-    //testMirrorGull();
-    //testMirrorDiagonal();
-    //testCollage();
-    //testCopy();
-    //testEdgeDetection();
-    //testEdgeDetection2();
-    //testChromakey();
-    //testEncodeAndDecode();
-    //testGetCountRedOverValue(250);
-    //testSetRedToHalfValueInTopHalf();
-    //testClearBlueOverValue(200);
+    // testZeroBlue();
+    // testKeepOnlyBlue(); 
+    testNegate(); 
+    // //testKeepOnlyBlue();
+    // // testKeepOnlyRed();
+    // // testKeepOnlyGreen();
+    // // testNegate();
+    // //testGrayscale();
+    // //testFixUnderwater();
+    // testMirrorVertical();
+    // testMirrorTemple();
+    // //testMirrorArms();
+    // //testMirrorGull();
+    // //testMirrorDiagonal();
+    // testCollage();
+    // // testCopy();
+    // testEdgeDetection();
+    // // testEdgeDetection2();
+    // //testChromakey();
+    // //testEncodeAndDecode();
+    // //testGetCountRedOverValue(250);
+    // //testSetRedToHalfValueInTopHalf();
+    // //testClearBlueOverValue(200);
     //testGetAverageForColumn(0);
   }
 }
